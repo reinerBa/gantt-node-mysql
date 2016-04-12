f25a99733d208c11a855949dee430bc8a0e8e590
AlexKlimenkov <shurick.klimenkov@gmail.com>
2016-04-12 16:44:53 +0300

    [fix] tasks table schema

--- schema.sql
+++ schema.sql
@@ -11,7 +11,6 @@ CREATE TABLE IF NOT EXISTS `gantt_tasks` (
   `text` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `start_date` datetime NOT NULL,
   `duration` int(11) NOT NULL,
-  `progress` float NOT NULL,
-  `sortorder` int(11) NOT NULL,
+  `progress` float NOT NULL DEFAULT 0,
   `parent` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
