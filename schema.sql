bcc8b4e9c9db2d1e0c3035a3ba5587de5e4e309a
reinerBa <reinerbam@mailbox.org>
2018-01-29 23:19:07 +0100

    Add project table

--- schema.sql
+++ schema.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS `gantt_links` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `source` int(11) NOT NULL,
   `target` int(11) NOT NULL,
-  `type` varchar(1) COLLATE utf8_unicode_ci NOT NULL
+  `type` varchar(1) COLLATE utf8_unicode_ci NOT NULL,
+  `project` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
@@ -12,5 +13,15 @@ CREATE TABLE IF NOT EXISTS `gantt_tasks` (
   `start_date` datetime NOT NULL,
   `duration` int(11) NOT NULL,
   `progress` float NOT NULL DEFAULT 0,
-  `parent` int(11) NOT NULL
+  `parent` int(11) NOT NULL,
+  `project` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `gantt_projects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `homepage` varchar(127) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `create_date` datetime NOT NULL,
+  `status` varchar(7) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'new'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
